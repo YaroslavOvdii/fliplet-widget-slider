6fd59faa6c09624d5c90fee7629acf41073a3007
Hugo Carneiro <hcarneiro@weboo.biz>
2016-11-16 16:34:28 +0000

    The dawn of Onboarding

---
+++ js/build.js
@@ -0,0 +1,37 @@
+$('.ob-skip').click(function (event) {
+  event.preventDefault();
+
+  var data = Fliplet.Widget.getData( $(this).parents('.onboarding-holder[data-onboarding-id]').attr('data-onboarding-id') );
+
+  if(!_.isUndefined(data) && (!_.isUndefined(data.skipLinkAction) && !_.isEmpty(data.skipLinkAction))) {
+    Fliplet.Navigate.to(data.skipLinkAction);
+  }
+});
+
+$('.btn[data-slide-button-id]').click(function (event) {
+  event.preventDefault();
+
+  var data = Fliplet.Widget.getData( $(this).parents('.onboarding-holder[data-onboarding-id]').attr('data-onboarding-id') );
+  var itemData = _.find(data.items,{id: $(this).data('slide-button-id')});
+
+  if(!_.isUndefined(itemData) && (!_.isUndefined(itemData.linkAction) && !_.isEmpty(itemData.linkAction))) {
+    Fliplet.Navigate.to(itemData.linkAction);
+  }
+});
+
+$('[data-onboarding-id]').each(function(){
+
+  var swiperElement = $(this).find('.swiper-container');
+  var swiper = new Swiper( swiperElement, {
+    direction: 'horizontal',
+    loop: false,
+    autoHeight: true,
+
+  	pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next span',
+    prevButton: '.swiper-button-prev span',
+    grabCursor: true
+  });
+
+});
---
+++ js/interface.js
@@ -0,0 +1,443 @@
+// VARS
+var widgetId = Fliplet.Widget.getDefaultId();
+var data = Fliplet.Widget.getData() || { items:[] },
+    linkPromises = [];
+
+data.items = data.items || [];
+
+_.forEach(data.items,function (item){
+  if(_.isObject(item.linkAction)) {
+    initItemLinkProvider(item);
+  }
+});
+
+if(_.isObject(data.skipLinkAction)) {
+  initSkipLinkProvider();
+}
+
+var listLength = data.items.length + 1;
+var accordionCollapsed = false;
+
+var $accordionContainer = $('#accordion');
+var templates = {
+  panel: template('panel')
+};
+
+enableAnimation();
+enableNavigation();
+enableSkipButton();
+checkPanelLength();
+
+setTimeout (function() {
+  // SORTING PANELS
+  $('.panel-group').sortable({
+    handle: ".panel-heading",
+    cancel: ".icon-delete",
+    tolerance: 'pointer',
+    revert: 150,
+    placeholder: 'panel panel-default placeholder tile',
+    cursor: '-webkit-grabbing; -moz-grabbing;',
+    axis: 'y',
+    start: function(event, ui) {
+      $('.panel-collapse.in').collapse('hide');
+      ui.item.addClass('focus').css('height', ui.helper.find('.panel-heading').outerHeight() + 2);
+      $('.panel').not(ui.item).addClass('faded');
+    },
+    stop: function(event, ui) {
+      ui.item.removeClass('focus');
+
+      var sortedIds = $( ".panel-group" ).sortable( "toArray" ,{attribute: 'data-id'});
+      data.items = _.sortBy(data.items, function(item){
+        return sortedIds.indexOf(item.id);
+      });
+      save();
+      $('.panel').not(ui.item).removeClass('faded');
+    },
+    sort: function(event, ui) {
+      $('.panel-group').sortable('refresh');
+      $('.tab-content').trigger('scroll');
+    }
+  });
+  $('form.form-horizontal').trigger('scroll');
+}, 1000);
+
+// EVENTS
+$(".tab-content")
+  .on('click', '.icon-delete', function() {
+
+    var $item = $(this).closest("[data-id], .panel"),
+        id = $item.data('id');
+
+    _.remove(data.items, {id: id});
+    _.remove(linkPromises,{id: id});
+
+    $(this).parents('.panel').remove();
+    checkPanelLength();
+    listLength--;
+    save();
+
+  })
+  .on('click', '.add-image', function() {
+
+    var $item = $(this).closest("[data-id], .panel"),
+        id = $item.data('id'),
+        item = _.find(data.items, {id: id});
+
+    initImageProvider(item);
+
+    $(this).text('Replace image');
+    if ( $(this).siblings('.thumb-holder').hasClass('hidden') ) {
+      $(this).siblings('.thumb-holder').removeClass('hidden');
+    }
+  })
+  .on('click', '.image-remove', function() {
+
+    var $item = $(this).closest("[data-id], .panel"),
+        id = $item.data('id'),
+        item = _.find(data.items, {id: id});
+
+    item.imageConf = null;
+    $(this).parents('.add-image-holder').find('.add-image').text('Add image');
+    $(this).parents('.add-image-holder').find('.thumb-holder').addClass('hidden');
+    save();
+  })
+  .on('click', '.list-item-set-link', function() {
+
+    var $item = $(this).closest("[data-id], .panel"),
+        id = $item.data('id'),
+        item = _.find(data.items, {id: id});
+
+    initItemLinkProvider(item);
+
+    $(this).siblings().removeClass('hidden');
+    $(this).addClass('hidden');
+    $(this).siblings('.link-remove').show();
+    save();
+
+  })
+  .on('click', '.set-skip-link', function() {
+
+    initSkipLinkProvider();
+
+    $(this).siblings().removeClass('hidden');
+    $(this).addClass('hidden');
+    $(this).siblings('.link-remove').show();
+    save();
+
+  })
+  .on('click', '.link-remove', function() {
+    var $item = $(this).closest("[data-id], .panel"),
+        id = $item.data('id'),
+        item = _.find(data.items, {id: id});
+
+    _.remove(linkPromises,{id: id});
+    $('[data-id="' + item.id + '"] .add-link').empty();
+    item.linkAction = null;
+    $(this).addClass('hidden');
+    $(this).siblings('.list-item-set-link').removeClass('hidden');
+    save();
+  })
+  .on('click', '.skip-link-remove', function() {
+    _.remove(linkPromises,{id: data.id});
+    $('.add-skip-link').empty();
+    data.skipLinkAction = null;
+    $(this).addClass('hidden');
+    $(this).siblings('.set-skip-link').removeClass('hidden');
+    save();
+  })
+  .on('keyup change blur paste', '.list-item-title', function() {
+    var $listItem = $(this).parents('.panel');
+    setListItemTitle($listItem.index(), $(this).val());
+
+    debounceSave();
+  }).on('keyup change blur paste', '.list-item-desc', function() {
+    debounceSave();
+  })
+  .on('keyup change blur paste', '.list-item-link-label', function() {
+    debounceSave();
+  })
+  .on('click', '.expand-items', function() {
+    var $panelCollapse = $('.panel-collapse.in');
+    // Update accordionCollapsed if all panels are collapsed/expanded
+    if (!$panelCollapse.length) {
+      accordionCollapsed = true;
+    } else if ($panelCollapse.length == $('.panel-collapse').length) {
+      accordionCollapsed = false;
+    }
+
+    if (accordionCollapsed) {
+      expandAccordion();
+    } else {
+      collapseAccordion();
+    }
+  })
+  .on('click', '.new-list-item', function() {
+
+    var item ={};
+    item.id = makeid(8);
+    item.number = listLength++;
+    item.linkAction = null;
+    item.description = "";
+    data.items.push(item);
+
+    addListItem(item);
+
+    checkPanelLength();
+    save();
+
+  })
+  .on('show.bs.collapse', '.panel-collapse', function() {
+    $(this).siblings('.panel-heading').find('.fa-chevron-right').removeClass('fa-chevron-right').addClass('fa-chevron-down');
+  })
+  .on('hide.bs.collapse', '.panel-collapse', function() {
+    $(this).siblings('.panel-heading').find('.fa-chevron-down').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+  })
+  .on('shown.bs.collapse hidden.bs.collapse', '.panel-collapse', function() {
+    $('.tab-content').trigger('scroll');
+  })
+  .on('change', 'input[name="enable_animation"]:radio', function() {
+    enableAnimation();
+  })
+  .on('change', 'input[name="enable_navigation"]:radio', function() {
+    enableNavigation();
+  })
+  .on('change', 'input[name="enable_skip"]:radio', function() {
+    enableSkipButton();
+  });
+
+$('#help_tip').on('click', function() {
+  alert("During beta, please use live chat and let us know what you need help with.");
+});
+
+var contentHeight = $('body > .form-horizontal').outerHeight();
+var tabPaneTopPadding = 78;
+
+$('body > .form-horizontal').scroll(function(event) {
+  var tabContentScrollPos = Math.abs($('.tab-pane-content').position().top - tabPaneTopPadding);
+  var tabPaneHeight = tabPaneTopPadding + $('.tab-pane-content').height();
+
+  if (tabPaneHeight - tabContentScrollPos > contentHeight) {
+    $('body').addClass('controls-sticky-on');
+  } else {
+    $('body').removeClass('controls-sticky-on');
+  }
+});
+
+// FUNCTIONS
+function enableAnimation() {
+  if ($('#enable-animation-yes').is(':checked')) {
+    data.animationEnabled = true;
+  } else if ($('#enable-animation-no').is(':checked')) {
+    data.animationEnabled = false;
+  }
+}
+
+function enableNavigation() {
+  if ($('#enable-navigation-yes').is(':checked')) {
+    data.navigationEnabled = true;
+  } else if ($('#enable-navigation-no').is(':checked')) {
+    data.navigationEnabled = false;
+  }
+}
+
+function enableSkipButton() {
+  if ($('#enable-skip-yes').is(':checked')) {
+    $('#skip-link').addClass('show');
+    data.skipEnabled = true;
+  } else if ($('#enable-skip-no').is(':checked')) {
+    $('#skip-link').removeClass('show');
+    data.skipEnabled = false;
+  }
+}
+
+function initItemLinkProvider(item){
+
+  item.linkAction = item.linkAction || {};
+  item.linkAction.provId = item.id;
+
+  var linkActionProvider = Fliplet.Widget.open('com.fliplet.link', {
+    // If provided, the iframe will be appended here,
+    // otherwise will be displayed as a full-size iframe overlay
+    selector: '[data-id="' + item.id + '"] .add-link',
+    // Also send the data I have locally, so that
+    // the interface gets repopulated with the same stuff
+    data: item.linkAction,
+    // Events fired from the provider
+    onEvent: function (event, data) {
+      if (event === 'interface-validate') {
+        Fliplet.Widget.toggleSaveButton(data.isValid === true);
+      }
+    },
+    closeOnSave: false
+  });
+
+  linkActionProvider.then(function (data) {
+    item.linkAction = data ? data.data: {};
+    return Promise.resolve();
+  });
+
+  linkActionProvider.id = item.id;
+  linkPromises.push(linkActionProvider);
+}
+
+function initSkipLinkProvider(){
+
+  data.skipLinkAction = data.skipLinkAction || {};
+
+  var skipLinkActionProvider = Fliplet.Widget.open('com.fliplet.link', {
+    // If provided, the iframe will be appended here,
+    // otherwise will be displayed as a full-size iframe overlay
+    selector: '.add-skip-link',
+    // Also send the data I have locally, so that
+    // the interface gets repopulated with the same stuff
+    data: data.skipLinkAction,
+    // Events fired from the provider
+    onEvent: function (event, data) {
+      if (event === 'interface-validate') {
+        Fliplet.Widget.toggleSaveButton(data.isValid === true);
+      }
+    },
+    closeOnSave: false
+  });
+
+  skipLinkActionProvider.then(function (result) {
+    data.skipLinkAction = result ? result.data: {};
+    return Promise.resolve();
+  });
+
+  skipLinkActionProvider.id = data.id;
+  linkPromises.push(skipLinkActionProvider);
+}
+
+var imageProvider;
+function initImageProvider(item){
+  imageProvider = Fliplet.Widget.open('com.fliplet.image-manager', {
+    // Also send the data I have locally, so that
+    // the interface gets repopulated with the same stuff
+    data: item.imageConf,
+    // Events fired from the provider
+    onEvent: function (event, data) {
+      if (event === 'interface-validate') {
+        Fliplet.Widget.toggleSaveButton(data.isValid === true);
+      }
+    },
+    single: true,
+    type: 'image'
+  });
+
+  Fliplet.Widget.toggleCancelButton(false);
+
+  window.addEventListener('message', function(event) {
+    if (event.data === 'cancel-button-pressed') {
+      Fliplet.Widget.toggleCancelButton(true);
+      imageProvider.close();
+      if(_.isEmpty(item.imageConf)) {
+        $('[data-id="' + item.id + '"] .add-image-holder').find('.add-image').text('Add image');
+        $('[data-id="' + item.id + '"] .add-image-holder').find('.thumb-holder').addClass('hidden');
+      }
+    }
+  });
+
+  Fliplet.Studio.emit('widget-save-label-update', {
+      text: 'Select & Save'
+  });
+
+  imageProvider.then(function (data) {
+    if(data.data) {
+      item.imageConf = data.data;
+      $('[data-id="' + item.id + '"] .thumb-image img').attr("src",data.data.thumbnail);
+      save();
+    }
+    imageProvider = null;
+    Fliplet.Studio.emit('widget-save-label-reset');
+    return Promise.resolve();
+  });
+}
+
+function template(name) {
+  return Handlebars.compile($('#template-' + name).html());
+}
+
+function makeid(length)
+{
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < length; i++ )
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+function expandAccordion(){
+  accordionCollapsed = false;
+  $('.panel-collapse').collapse('show');
+}
+
+function collapseAccordion(){
+  accordionCollapsed = true;
+  $('.panel-collapse').collapse('hide');
+}
+
+function setListItemTitle(index, title) {
+  $('#accordion').find('.panel:eq(' + index + ') .panel-title-text').html(title);
+}
+
+function addListItem(data) {
+  var $newPanel = $(templates.panel(data));
+  $accordionContainer.append($newPanel);
+
+  $newPanel.find('.form-control:eq(0)').select();
+  $('form.form-horizontal').stop().animate({
+    scrollTop: $('.tab-content').height()
+  }, 300, function(){
+    $('form.form-horizontal').trigger('scroll');
+  });
+}
+
+function checkPanelLength() {
+  if ( $('.panel').length ) {
+    $('#slides').removeClass('list-items-empty');
+  } else {
+    $('#slides').addClass('list-items-empty');
+  }
+}
+
+Fliplet.Widget.onSaveRequest(function () {
+  if(imageProvider){
+    imageProvider.forwardSaveRequest();
+  } else {
+    save(true);
+  }
+});
+
+var debounceSave = _.debounce( save, 500);
+
+function save(notifyComplete){
+  _.forEach(data.items,function(item){
+    item.description = $('#list-item-desc-'+item.id).val();
+    item.title = $('#list-item-title-'+item.id).val();
+    item.linkLabel = $('#list-item-link-label-'+item.id).val();
+  });
+
+  if(notifyComplete) {
+    Fliplet.Widget.all(linkPromises).then(function () {
+      // when all providers have finished
+      Fliplet.Widget.save(data).then(function () {
+        // Close the interface for good
+        Fliplet.Studio.emit('reload-widget-instance', widgetId);
+        Fliplet.Widget.complete();
+      });
+    });
+
+    // forward save request to all providers
+    linkPromises.forEach(function (promise) {
+      promise.forwardSaveRequest();
+    });
+  } else {
+    // Partial save while typing/using the interface
+    Fliplet.Widget.save(data).then(function () {
+      Fliplet.Studio.emit('reload-widget-instance', widgetId);
+    });
+  }
+}
