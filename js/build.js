64ef242e25889bed3f6c62c00503a14a9a3f40a5
hcarneiro <hcarneiro@weboo.biz>
2018-02-12 17:06:54 +0000

    Fixes issue with multiple sliders on the screen

--- js/build.js
+++ js/build.js
@@ -1,39 +1,41 @@
-$('.ob-skip span').click(function () {
-  var data = Fliplet.Widget.getData( $(this).parents('.onboarding-holder').data('onboarding-id') );
-
-  if(!_.isUndefined(data) && (!_.isUndefined(data.skipLinkAction) && !_.isEmpty(data.skipLinkAction))) {
-    Fliplet.Navigate.to(data.skipLinkAction);
-  }
-});
-
-$('.btn[data-slide-button-id]').click(function (event) {
-  event.preventDefault();
-
-  var data = Fliplet.Widget.getData( $(this).parents('.onboarding-holder').data('onboarding-id') );
-  var itemData = _.find(data.items,{id: $(this).data('slide-button-id')});
-
-  if(!_.isUndefined(itemData) && (!_.isUndefined(itemData.linkAction) && !_.isEmpty(itemData.linkAction))) {
-    Fliplet.Navigate.to(itemData.linkAction);
-  }
-});
-
 function init(){
   $('[data-onboarding-id]').each(function(){
-
-    var swiperElement = $(this).find('.swiper-container');
+    var container = this;
+    var id = $(container).data('onboarding-id');
+    var config = Fliplet.Widget.getData(id);
+    var swiperElement = $(container).find('.swiper-container');
     var swiper = new Swiper( swiperElement, {
       direction: 'horizontal',
       loop: false,
       autoHeight: true,
 
-      pagination: '.swiper-pagination',
+      pagination: '.swiper-pagination-' + id,
       paginationClickable: true,
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
+      nextButton: '.swiper-button-next-' + id,
+      prevButton: '.swiper-button-prev-' + id,
       grabCursor: true
     });
 
     swiper.updateContainerSize();
+
+    $(container).find('.ob-skip span').click(function () {
+      var data = Fliplet.Widget.getData( $(this).parents('.onboarding-holder').data('onboarding-id') );
+
+      if(!_.isUndefined(data) && (!_.isUndefined(data.skipLinkAction) && !_.isEmpty(data.skipLinkAction))) {
+        Fliplet.Navigate.to(data.skipLinkAction);
+      }
+    });
+
+    $(container).find('.btn[data-slide-button-id]').click(function (event) {
+      event.preventDefault();
+
+      var data = Fliplet.Widget.getData( $(this).parents('.onboarding-holder').data('onboarding-id') );
+      var itemData = _.find(data.items,{id: $(this).data('slide-button-id')});
+
+      if(!_.isUndefined(itemData) && (!_.isUndefined(itemData.linkAction) && !_.isEmpty(itemData.linkAction))) {
+        Fliplet.Navigate.to(itemData.linkAction);
+      }
+    });
 
   });
 }
