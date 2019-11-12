a3eaf129f4d8c383c4520c37d67fd166203318d5
YaroslavOvdii <yaroslav.o@upplabs.com>
2019-11-12 12:59:34 +0200

    remove imageProvider on cancel

--- js/interface.js
+++ js/interface.js
@@ -259,10 +259,13 @@ var FlSlider = (function() {
         if (event.data === 'cancel-button-pressed') {
           Fliplet.Widget.toggleCancelButton(true);
           imageProvider.close();
+          
           if (_.isEmpty(item.imageConf)) {
             $('[data-id="' + item.id + '"] .add-image-holder').find('.add-image').text('Add image');
             $('[data-id="' + item.id + '"] .add-image-holder').find('.thumb-holder').addClass('hidden');
           }
+
+          imageProvider = null;
         }
       });
 
